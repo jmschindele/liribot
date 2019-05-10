c9b3ffb001b22e597021f17c6589b2b6c5591651
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-05-10 14:38:06 -0400

    updated read me to include video and screen shots

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-Liri-bot uses APIs from Spotify to get songs, Bands in Town to get concerts and OMDB to get movies.
+Liri-bot is a CLI App driven by Node.js that uses APIs from Spotify to get songs, Bands in Town to get concerts and OMDB to get movies. Axios is used to make these API calls.
 
-Axios is used to make the API calls.
+Calls that are made are stored to a log.txt file using npm's fs and the application can also read the contents of a file entitled random.txt by parsing out the commands into a switch case.
 
+
+How it works:
+
+1. The use runs the program by calling node liri "command" "arguments/details about the command"
+
+The command must be entered as a hyphenated sentence and is stored as a variable, which then uses a switch case to initiate the appropriate function:
+
+![](2019-05-10-14-28-13.png)
+
+2. Each function is structured similarly and just parses out the object data to return the users desired result.
+
+3. The do-what-it-says command calls the random() function, which then parses out the data from the random.txt file and repeats the switch.
+
+
+Video Demonstration:
+
+https://drive.google.com/file/d/1VRir_1OYNuA-JzKdJ1mWt2WpU0V1avlX/view
 
--- liri.js
+++ liri.js
@@ -1,89 +1,41 @@
 require("dotenv").config();
-
-// TODO Grab the axios package...
-// @link https://www.npmjs.com/package/axios
-
+//Call node packages
 var axios = require("axios");
-var inquirer = require("inquirer");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
-// var moment = require("moment.js");
+var moment = require("moment");
 
+// store user inputs in variables
 var command = process.argv[2];
 var input = process.argv.slice(3).join("+");
 
-
-
+//build axios urls based on user input
 var concertURL =
   "https://rest.bandsintown.com/artists/" +
   input +
   "/events?app_id=codingbootcamp";
-
 var movieURL =
   "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
 
+  //take the users command and execute the corresponding function
 switch (command) {
-  case "concert-this":
-    axios.get(concertURL).then(function(response) {
-      console.log(response.data[0].venue.name +
-      
-        response.data[0].venue.city +
-          ", " +
-          response.data[0].venue.region +
-          " " +
-          response.data[0].venue.country
-      );
-    });
+  case "concert-this": 
+    concert();
     break;
+
   case "spotify-this-song":
-    if (input === "") {
-      input = "The Sign Ace of Base";
-    }
-    spotify
-      .search({ type: "track", query: input })
-      .then(function(response) {
-        console.log(response.tracks.items[0]);
-        console.log(response.tracks.items[0].album.name);
-        console.log(response.tracks.items[0].album.artists[0].name);
-        console.log(response.tracks.items[0].available_markets[0].disc_number);
-
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-
+    songCall();
     break;
+
   case "movie-this":
-    if (input === "") {
-      movieURL =
-        "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
-    }
-    axios.get(movieURL).then(function(response) {
-      console.log(
-        "\r",
-        response.data.Title,
-        "\n\r",
-        response.data.Year,
-        "\n\r",
-        "IMDB Rating: ",
-        response.data.Ratings[0].Value,
-        "\n\r Rotten Tomatoes: ",
-        response.data.Ratings[1].Value,
-        "\n\r Country: ",
-        response.data.Country,
-        "\n\r Language: ",
-        response.data.Language,
-        "\n\r Plot: ",
-        response.data.Plot,
-        "\n\r Actors: ",
-        response.data.Actors
-      );
-    });
+    movieCall();
     break;
+
+
   case "do-what-it-says":
-    console.log("switch 4");
+    random();
     break;
 
   default:
@@ -91,3 +43,131 @@ switch (command) {
 }
 
 
+// make an API call to bands in town, display the artist, venue and location.
+function concert() {
+  axios.get(concertURL).then(function(response) {
+    var concert = response.data[0];
+    var concertArr = [
+      "",
+      "Artist: " + input.toUpperCase(),
+      "Venue: " + concert.venue.name,
+      "Location: " +
+        concert.venue.city +
+        " " +
+        concert.venue.region +
+        " " +
+        concert.venue.country,
+      "_".repeat(60)
+    ].join("\n\n");
+    console.log(concertArr);
+    // console.log(concert)
+    fs.appendFile("log.txt", concertArr, function(err) {
+      if (err) throw err;
+    });
+  });
+}
+
+// spotify call - display the song, artist, album and a preview link. Default to The Sign by Ace of Base if no user entry
+
+function songCall() {
+  if (input === "") {
+    input = "The Sign Ace of Base";
+  }
+  spotify
+    .search({ type: "track", query: input })
+    .then(function(response) {
+      var song = response.tracks.items[0];
+      var songArr = [
+        "",
+        "Song: " + response.tracks.items[0].name,
+        "Artist: " + song.album.artists[0].name,
+
+        "Album: " + song.album.name,
+
+        "Preview: " + song.preview_url,
+        "_".repeat(60)
+      ].join("\n\n");
+      console.log(songArr);
+      fs.appendFile("log.txt", songArr, function(err) {
+        if (err) throw err;
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+
+// OMdB call. Default to Mr Nobody if no entry from user.
+
+function movieCall() {
+  if (input === "") {
+    movieURL =
+      "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
+  }
+  axios.get(movieURL).then(function(response) {
+    var movie = response.data;
+    var movieArr = [
+      "",
+      "Movie: " + movie.Title,
+      "Movie Year: " + movie.Year,
+      "IMDB Rating: " + movie.Ratings[0].Value,
+      "Rotten Tomatoes: " + movie.Ratings[1].Value,
+      "Country: " + movie.Country,
+      "Language: " + movie.Language,
+      "Plot: " + movie.Plot,
+      "Actors: " + movie.Actors,
+      "_".repeat(60),
+      ""
+    ].join("\n\n");
+    console.log(movieArr);
+    fs.appendFile("log.txt", movieArr, function(err) {
+      if (err) throw err;
+    });
+  });
+}
+
+// do what it says line - this calls the random.txt file and performs the command on it
+function random() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) throw err;
+
+    var dataArr = data.split(",");
+    command = dataArr[0];
+    input = dataArr[1];
+
+    switch (command) {
+      case "concert-this":
+        var concertURL =
+          "https://rest.bandsintown.com/artists/" +
+          input +
+          "/events?app_id=codingbootcamp";
+        concert();
+        break;
+
+      case "spotify-this-song":
+        songCall();
+        break;
+      case "movie-this":
+        movieURL =
+          "http://www.omdbapi.com/?t=" +
+          input +
+          "&y=&plot=short&apikey=trilogy";
+        console.log(command, input);
+        movieCall();
+        break;
+    }
+  });
+}
+
+
+/* perform the following:
+
+node liri concert-this Garth Brooks
+node liri spotify-this-song all the small things
+node liri spotify-this-song // returns I Saw the Sign by Ace of Bass
+node liri movie-this blade runner
+node liri movie-this //returns to mr nobody
+node liri do-what-it-says //runs the command in random.txt(currently movie-this shrek)
+
+*/
